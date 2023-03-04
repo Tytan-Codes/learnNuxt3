c6642a512f5687827f3dfb3faf0ba4a0a0f7dddb
Tytan <tytan@thetytan.com>
2023-03-03 19:22:12 -0800

    asdasdsdds

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        
+        <h1>about</h1>
+        <p>lorum stuff is here</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px, 0;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>lorum </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px, 0;
+    }
+</style>
